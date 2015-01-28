4f55ca747a61ebf21eba3fd00beac80052ae209a
Philip Murphy <philipjmurphy@gmail.com>
2015-01-28 00:36:09 +0000

    Rename app_bootstrap.js and apply JS Lint fix.

--- js/app-bootstrap.js
+++ js/app-bootstrap.js
@@ -3,7 +3,7 @@ var app = {};
 app.host = 'http://api.dev.queue.appbucket.eu/';
 app.queuesUrl = app.host + 'queues/';
 app.queuesDetailsUrl = app.queuesUrl + 'details/';
-app.feedbackUrl = app.host + 'feedbacks/'
+app.feedbackUrl = app.host + 'feedbacks/';
 
 // Required for Ajax to work on IE.
 $.support.cors = true;
